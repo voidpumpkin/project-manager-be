1b8cc03afcd91b1b7cb9e0d3c2770766a82c334a
voidpumpkin <32368314+voidpumpkin@users.noreply.github.com>
2019-09-25 17:49:49 +0300

    feat(#9): koa-logger

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const Koa = require('koa');
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
 
+// logger
+const logger = require('koa-logger');
+const transporter = process.env.LOG_REQ === 'true' ? str => console.log(str) : () => {};
+app.use(logger(transporter));
+
 // sessions
 const session = require('koa-session');
 app.keys = ['voidpumpkin-session-secret'];
